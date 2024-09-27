7257d882e056a29fa4e76e8dd79a596eea89e404
BrandoAp <avilabrando14@gmail.com>
2024-09-26 23:27:23 -0500

    Modificacion del lab 4 a lab 5

--- Laboratorio-4/js/app.js
+++ Laboratorio-4/js/app.js
@@ -1,27 +1,46 @@
-const App = (() => {
-    const htmlElement = {
-        form : document.querySelector('form'),
-        cadena : document.querySelector('#cadena'),
-        response : document.querySelector('#response')
-    }
+const App = (({ reverseString }) => {
+  const htmlElement = {
+    form: document.querySelector("form"),
+    response: document.querySelector("#response"),
+    colorSelect: document.querySelector("#color"),
+  };
 
-    const handlers = {
-        onForm(e){
-            //e.preventDefault();
-            const cadena = htmlElement.cadena.value;
-            htmlElement.response.innerHTML = `<span>${cadena.split('').reverse().join('')}</span>`
-        }
-    }
+  const handlers = {
+    onForm(e) {
+      //e.preventDefault();
+      htmlElement.response.textContent = reverseString(e.target.value);
+    },
+    onColorChange() {
+      htmlElement.response.classList.remove(
+        "color-black",
+        "color-blue",
+        "color-gray"
+      );
 
-    const bindEvents = () => {
-        htmlElement.form.addEventListener('keydown', handlers.onForm);
-        }
+      const selectedColor = htmlElement.colorSelect.value;
+      if (selectedColor === "black") {
+        htmlElement.response.classList.add("color-black");
+      } else if (selectedColor === "blue") {
+        htmlElement.response.classList.add("color-blue");
+      } else if (selectedColor === "gray") {
+        htmlElement.response.classList.add("color-gray");
+      }
+    },
+  };
 
-    return {
-        init(){
-            bindEvents();
-        },
-    };
-})();
+  const bindEvents = () => {
+    htmlElement.form.elements.cadena.addEventListener(
+      "keydown",
+      handlers.onForm
+    );
+    htmlElement.colorSelect.addEventListener('change', handlers.onColorChange);
+  };
+
+  return {
+    init() {
+      bindEvents();
+    },
+  };
+})(window.Utils);
 
 App.init();
