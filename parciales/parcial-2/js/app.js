1ec323df0f11ea599d9ee43c2960223ca37ca9e7
BrandoAp <avilabrando14@gmail.com>
2024-10-10 11:04:49 -0500

    parcial 2 terminado

--- parciales/parcial-2/js/app.js
+++ parciales/parcial-2/js/app.js
@@ -1,54 +1,64 @@
 const App = (() => {
-    const htmlElements= {
-        generatedButton : document.querySelector('#add-button'),
-        ascSortedButton : document.querySelector('#asc-button-sorted'),
-        descSortedButton : document.querySelector('#desc-button-sorted'),
-        listNumber : document.querySelector('#list-numbers') 
-    }
+  const htmlElements = {
+    generatedButton: document.querySelector("#add-button"),
+    ascSortedButton: document.querySelector("#asc-button-sorted"),
+    descSortedButton: document.querySelector("#desc-button-sorted"),
+    listNumber: document.querySelector("#list-numbers"),
+  };
 
-    let numbers = [];
+  let numbers = [];
 
-    const handles = {
-        onClickButtonGenerated(e) {
-            let randomNumber = Math.floor(Math.random() * 100);
-            if (!numbers.includes(randomNumber)) {
-                numbers.push(randomNumber)
-            }
-            displayNumber();
-        },
-        onClickButtonAsc(e){
-            numbers.sort((a,b) => a - b);
-            displayNumber();
-        },
-        onClickButtonDesc(e){   
-            numbers.sort((a,b)=> b - a);
-            displayNumber();
-        }
-    }
+  const handles = {
+    onClickButtonGenerated(e) {
+      let randomNumber = Math.floor(Math.random() * 99) + 1;
+      if (!numbers.includes(randomNumber)) {
+        numbers.push(randomNumber);
+      } else {
+        alert("El numero " + randomNumber + " ya fue generado");
+      }
+      displayNumber();
+    },
+    onClickButtonAsc(e) {
+      numbers.sort((a, b) => a - b);
+      displayNumber();
+    },
+    onClickButtonDesc(e) {
+      numbers.sort((a, b) => b - a);
+      displayNumber();
+    },
+  };
 
-    const displayNumber = () => {
-        const numberList = htmlElements.listNumber;
-        numberList.innerHTML = '';
+  const displayNumber = () => {
+    const numberList = htmlElements.listNumber;
+    numberList.innerHTML = "";
 
-        numbers.forEach(num => {
-            const li = document.createElement('li');
-            li.textContent = num;
-             numberList.appendChild(li);
-        })
-        
-    }
+    numbers.forEach((num) => {
+      const li = document.createElement("li");
+      li.textContent = num.toString().padStart(2, "0");
+      numberList.appendChild(li);
+    });
+  };
 
-    const bindEvent = () => {
-        htmlElements.generatedButton.addEventListener('click', handles.onClickButtonGenerated);
-        htmlElements.ascSortedButton.addEventListener('click', handles.onClickButtonAsc);
-        htmlElements.descSortedButton.addEventListener('click', handles.onClickButtonDesc);
-    }
+  const bindEvent = () => {
+    htmlElements.generatedButton.addEventListener(
+      "click",
+      handles.onClickButtonGenerated
+    );
+    htmlElements.ascSortedButton.addEventListener(
+      "click",
+      handles.onClickButtonAsc
+    );
+    htmlElements.descSortedButton.addEventListener(
+      "click",
+      handles.onClickButtonDesc
+    );
+  };
 
-    return {
-        init() {
-            bindEvent();
-        }
-    };
+  return {
+    init() {
+      bindEvent();
+    },
+  };
 })();
 
 App.init();
